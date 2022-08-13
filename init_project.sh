cede6ddf88c2224e47750b9d91b22687a6698eae
Pipe Galera <pipegalera@gmail.com>
2022-08-13 11:50:23 +0200

    Initial commit

---
+++ init_project.sh
@@ -0,0 +1,9 @@
+mkdir ml_project
+cd ml_project
+mkdir src input notebooks models 
+touch README.md .gitignore
+cd src 
+touch create_folds.py train.py inference.py models.py config.py model_dispatcher.py
+cd ..
+cd notebooks
+touch exploration.ipynb check_data.ipynb
